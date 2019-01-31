39daa64c1c9ba6216f88417282586e10fcf8580b
test <test@gmail.com>
2019-01-31 23:45:17 +0530

    Add project files.

---
+++ MultipleProjects_git/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MultipleProjects_git.MvcApplication" Language="C#" %>
---
+++ MultipleProjects_git/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MultipleProjects_git.Startup))]
+namespace MultipleProjects_git
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
