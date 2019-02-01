8d2a0a2cd3a2de0c3d1dce44875ec984c9a263a6
test <test@gmail.com>
2019-02-01 08:49:08 +0530

    bd

---
+++ DomainModel/MasterB.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainModel
+{
+    class MasterB
+    {
+        //test
+
+        public void mastermethodB()
+        {
+            // master methodC
+        }
+        public void mastermethod2B()
+        {
+            // master method2C
+        }
+    }
+
+
+}
